ab8f8ca6352523979b4baeb16678d9e175bdb83d
Kristian Kielhofner <kris@tovera.com>
2024-06-11 08:26:46 -0500

    Add torch_info script

---
+++ misc/torch_info.py
@@ -0,0 +1,34 @@
+import torch
+import os
+
+def report_env():
+    rocr_devices = os.getenv("ROCR_VISIBLE_DEVICES")
+    hip_devices = os.getenv("HIP_VISIBLE_DEVICES")
+    cuda_visible_devices = os.getenv("CUDA_VISIBLE_DEVICES")
+    torch_version = torch.__version__
+    cuda_available = torch.cuda.is_available()
+    curr_device = torch.cuda.current_device()
+    device_arch = str(torch.cuda.get_device_name(torch.cuda.current_device()))
+    cuda_version = torch.version.cuda
+    hip_version = torch.version.hip
+    bf16_support = torch.cuda.is_bf16_supported()
+    nccl_available = torch.distributed.is_nccl_available()
+    nccl_version = torch.cuda.nccl.version()
+    cuda_device_count = torch.cuda.device_count()
+    print(f"Torch version: {torch_version}")
+    print(f"CUDA available: {cuda_available} ")
+    print(f"CUDA version: {cuda_version} ")
+    print(f"HIP version: {hip_version} ")
+    print(f"Current device: {curr_device} ")
+    print(f"Device arch name: {device_arch} ")
+    print(f"CUDA Device count: {cuda_device_count} ")
+    print(f"BF16 support: {bf16_support} ")
+    print(f"NCCL available: {nccl_available} ")
+    print(f"NCCL version: {nccl_version} ")
+    print(f"ROCR_VISIBLE_DEVICES: {rocr_devices} ")
+    print(f"HIP_VISIBLE_DEVICES: {hip_devices} ")
+    print(f"CUDA_VISIBLE_DEVICES: {cuda_visible_devices} ")
+
+report_env()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
