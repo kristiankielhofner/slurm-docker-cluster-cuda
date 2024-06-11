2fd95b832ebc9f23a36cfd863723838eaf3ee38e
Kristian Kielhofner <kris@atomic-canyon.com>
2024-06-11 16:43:24 +0000

    More debug for torch_info.py

--- misc/torch_info.py
+++ misc/torch_info.py
@@ -1,7 +1,15 @@
 import torch
 import os
+import socket
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
 def report_env():
+    hostname = socket.gethostname()
+    ip_address = get_ip_address()
     rocr_devices = os.getenv("ROCR_VISIBLE_DEVICES")
     hip_devices = os.getenv("HIP_VISIBLE_DEVICES")
     cuda_visible_devices = os.getenv("CUDA_VISIBLE_DEVICES")
@@ -15,6 +23,8 @@ def report_env():
     nccl_available = torch.distributed.is_nccl_available()
     nccl_version = torch.cuda.nccl.version()
     cuda_device_count = torch.cuda.device_count()
+    print(f"Hostname: {hostname}")
+    print(f"IP Address: {ip_address}")
     print(f"Torch version: {torch_version}")
     print(f"CUDA available: {cuda_available} ")
     print(f"CUDA version: {cuda_version} ")
@@ -32,3 +42,4 @@ def report_env():
 report_env()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
+print()
